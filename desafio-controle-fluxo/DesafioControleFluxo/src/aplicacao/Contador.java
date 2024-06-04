90401529bbf628b8f57f6198be646e134cf02a22
dbengp <167815532+dbengp@users.noreply.github.com>
2024-06-04 09:08:31 -0300

    fix: code imports and structure

--- desafio-controle-fluxo/DesafioControleFluxo/src/aplicacao/Contador.java
+++ desafio-controle-fluxo/DesafioControleFluxo/src/aplicacao/Contador.java
@@ -1,32 +1,35 @@
 package aplicacao;
 
+import java.util.Scanner;
+
+import java.util.InputMismatchException;
 import excecoes.ParametrosInvalidosException;
 
 public class Contador {
     public static void main(String[] args) throws Exception {
 
         Scanner terminal = new Scanner(System.in);
-		System.out.println("Digite o primeiro parâmetro");
+	try {
+	        System.out.println("Digite o primeiro parâmetro");
 		int parametroUm = terminal.nextInt();
 		System.out.println("Digite o segundo parâmetro");
 		int parametroDois = terminal.nextInt();
-        		
-		try {
-            contar(parametroUm, parametroDois);
-		
-		}catch (ParametrosInvalidosException e) {
+                contar(parametroUm, parametroDois);
+	     }catch (ParametrosInvalidosException e) {
 			System.err.println(e.getMessage());
+	     }catch (InputMismatchException e) {
+			System.err.println("Digite valores inteiros!");
 		}
-    }
-}
-public static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+   }
+   public static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 	
-    if(parametroUm > parametroDois) {
-        throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
-    }else {
-        int contagem = parametroDois - parametroUm;
-        for(int i = 1; i <= contagem; i++){
-            System.out.println("Imprimindo o número " + i);
-        }
-    }
+	    if(parametroUm > parametroDois) {
+	        throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+	    }else {
+	        int contagem = parametroDois - parametroUm;
+	        for(int i = 1; i <= contagem; i++){
+	            System.out.println("Imprimindo o número " + i);
+	        }
+	    }
+   }
 }
